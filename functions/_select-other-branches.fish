6b85c958dc82be8a7934520541783bd7631530f3
Sam Kagan <skagan@nrao.edu>
2024-05-09 10:36:27 -0600

    Sorted git branches by commit date, descending

--- functions/_select-other-branches.fish
+++ functions/_select-other-branches.fish
@@ -5,13 +5,14 @@ function _select-other-branches --description "Select git branch(es) other than 
     set -l options (fish_opt -s m -l multi)
     set -a options (fish_opt -s r -l remote)
     set -a options (fish_opt -s q -l query --required-val)
-    argparse $options -- $argv; or return  # Exit if arg-parsing fails
+    argparse $options -- $argv; or return # Exit if arg-parsing fails
     if test -n $_flag_query
         set query_flag --query=$_flag_query
     end
-    
-    git branch --list $_flag_remote |
-    rg --invert-match '\*|\->' |  # Exclude current branch and remote HEADs
-    cut -c 3- |
-    fzf $query_flag $_flag_multi --preview='git log {} --'
+
+    git branch --sort=committerdate --list $_flag_remote |
+        tac |
+        rg --invert-match '\*|\->' | # Exclude current branch and remote HEADs
+        cut -c 3- |
+        fzf $query_flag $_flag_multi --preview='git log {} --'
 end
