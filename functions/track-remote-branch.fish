116b805b457298743ff1f0207a8e1773c0063f10
Sam Kagan <skagan@nrao.edu>
2024-07-22 12:24:29 -0600

    Updated track-remote-branch to fetch first

--- functions/track-remote-branch.fish
+++ functions/track-remote-branch.fish
@@ -2,18 +2,19 @@
 # Date modified: 5/17/2021
 
 function track-remote-branch --description "Create a git branch that tracks a selected remote branch"
+    # Because local copies of remote branches aren't kept up-to-date automatically
+    git fetch
     if test (count $argv) -gt 0
         set query_flag --query=$argv[(count $argv)]
     end
 
-    set -l remote_branch (_select-other-branches $query_flag --remote)  # Should always be a valid Git branch name
-    set -l local_branch (echo $remote_branch | sd '^.+/' '')  # remote_branch is valid ==> local_branch is valid
-    
+    set -l remote_branch (_select-other-branches $query_flag --remote) # Should always be a valid Git branch name
+    set -l local_branch (echo $remote_branch | sd '^.+/' '') # remote_branch is valid ==> local_branch is valid
+
     if test -n $local_branch
+        echo
         git branch $local_branch -t $remote_branch
         git switch $local_branch
-        # Because local copies of remote branches aren't kept up-to-date automatically
-        git pull
     else
         return 1
     end
