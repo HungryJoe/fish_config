b9da686cb39a107ea901f0fe5db902f359a7f9e8
Sam Kagan <skagan@nrao.edu>
2024-11-12 16:59:29 -0700

    Added --no-fetch option to track-remote-branch

--- functions/track-remote-branch.fish
+++ functions/track-remote-branch.fish
@@ -2,10 +2,15 @@
 # Date modified: 5/17/2021
 
 function track-remote-branch --description "Create a git branch that tracks a selected remote branch"
-    # Because local copies of remote branches aren't kept up-to-date automatically
-    git fetch
-    if test (count $argv) -gt 0
-        set query_flag --query=$argv[(count $argv)]
+    set -l options (fish_opt -s n -l no-fetch)
+    set -a options (fish_opt -s q -l query --required-val)
+    argparse $options -- $argv; or return # Exit if arg-parsing fails
+    if test -n $_flag_query
+        set query_flag --query=$_flag_query
+    end
+    if test -z $_flag_no_fetch
+        # Because local copies of remote branches aren't kept up-to-date automatically
+        git fetch
     end
 
     set -l remote_branch (_select-other-branches $query_flag --remote) # Should always be a valid Git branch name
